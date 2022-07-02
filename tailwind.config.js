6cc98e8fcd06a54553560ed9ed688be236e1d7fe
Shakhawat <shsourov2000@gmail.com>
2022-07-02 21:32:00 +0600

    basic setup talwindcss for vanzer-portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Maven': ['Maven Pro', 'sans-serif']
+      },
+      backgroundImage: {
+        'hero': "url('./../images/home.png')",
+      }
+    },
+  },
+  plugins: [],
+}
